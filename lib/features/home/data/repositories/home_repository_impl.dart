c1b8e4afc307e4dfba285fdcb8a12dbdf3370534
robi <robi3443@gmail.com>
2024-07-25 00:16:04 +0600

    Extract youtube url

--- lib/features/home/data/repositories/home_repository_impl.dart
+++ lib/features/home/data/repositories/home_repository_impl.dart
@@ -1,12 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:intl/intl.dart';
+import 'package:sfl_media/core/logger.dart';
 import 'package:sfl_media/core/network/constants/constants.dart';
 import 'package:sfl_media/core/network/constants/typedefs.dart';
 import 'package:sfl_media/features/home/data/api/news_api.dart';
 import 'package:sfl_media/features/home/domain/entities/news.dart';
 import 'package:sfl_media/features/home/domain/repositories/home_repository.dart';
-import 'package:sfl_media/dummy_data.dart' as d;
 
 @Injectable(as: HomeRepository)
 class HomeRepositoryImpl extends HomeRepository {
@@ -18,17 +18,19 @@ class HomeRepositoryImpl extends HomeRepository {
   AsyncResult<List<News>> fetchNews(String categoryId) async {
     List<News> newsList = [];
     final response = await newsApi.get(parameters: {'categories=': categoryId});
-
+    const image =
+        'https://sfl.media/wp-content/uploads/2024/07/2024-european-championships-round-of-16-3592df36-7075-46b7-aeae-77901ec71c19.jpeg';
     return response.fold((result) {
       for (var news in result.list) {
+        Log.info(extractYouTubeUrl(news.content.rendered ?? '') ?? '');
         newsList.add(
           News(
-            title: news.title.rendered ?? '',
-            description: Bidi.stripHtmlIfNeeded(news.content.rendered ?? ''),
-            thumbnailImage: d.newsList[0].thumbnailImage,
-            author: 'Patrick',
-            date: news.date,
-          ),
+              title: news.title.rendered ?? '',
+              description: Bidi.stripHtmlIfNeeded(news.content.rendered ?? ''),
+              thumbnailImage: image,
+              author: news.author.toString(),
+              date: news.date,
+              videoUrl: extractYouTubeUrl(news.content.rendered ?? '')),
         );
       }
 
@@ -36,5 +38,23 @@ class HomeRepositoryImpl extends HomeRepository {
     }, (r) {
       return const Right(defaultFailureMessage);
     });
+  }
+
+  String? extractYouTubeUrl(String htmlContent) {
+    RegExp regex = RegExp(r'<iframe.*?src=["\"](.*?)["\"]');
+
+    Match? match = regex.firstMatch(htmlContent);
+
+    if (match != null) {
+      String? url = match.group(1);
+
+      if (url != null) {
+        if (url.contains('youtube.com') || url.contains('youtu.be')) {
+          return url;
+        }
+      }
+    }
+
+    return null;
   }
 }
